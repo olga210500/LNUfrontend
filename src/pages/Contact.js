7414068ed6b0825c8004fdc4625f80b4867d2e03
olga210500 <olakovalisko@gmail.com>
2021-11-11 19:03:50 +0200

    codeowners

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -22,21 +22,21 @@ export default function Contact() {
                                     <div class="info-box">
                                         <i class="bx bx-map" > <Icons.Map/></i>
                                         <h3>Our Address</h3>
-                                        <p>A108 Adam Street, New York, NY 535022</p>
+                                        <p>вулиця Університетська, 1<br/>Львів, Львівська область, 79000</p>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="info-box">
                                         <i class="bx bx-envelope"><Icons.Envelope/></i>
                                         <h3>Email Us</h3>
-                                        <p>info@example.com<br /></p>
+                                        <p>ami.faculty@lnu.edu.ua<br /></p>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="info-box">
                                         <i class="bx bx-phone-call"><Icons.Telephone/></i>
                                         <h3>Call Us</h3>
-                                        <p>+1 5589 55488 55<br /></p>
+                                        <p>(032)-26-034-02<br /></p>
                                     </div>
                                 </div>
                             </div>
@@ -71,7 +71,8 @@ export default function Contact() {
                     </div>
 
                 </div>
-            </section><section class="map mt-2">
+            </section>
+            <section class="map mt-2">
                     <div class="container-fluid p-0">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.071218229993!2d24.020492876617606!3d49.84111933790114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473add717532cff9%3A0x1ea627f45b408179!2z0JvRjNCy0ZbQstGB0YzQutC40Lkg0L3QsNGG0ZbQvtC90LDQu9GM0L3QuNC5INGD0L3RltCy0LXRgNGB0LjRgtC10YIg0ZbQvNC10L3RliDQhtCy0LDQvdCwINCk0YDQsNC90LrQsA!5e0!3m2!1suk!2sua!4v1636473718100!5m2!1suk!2sua" allowfullscreen="" loading="lazy"></iframe>
                     </div>
