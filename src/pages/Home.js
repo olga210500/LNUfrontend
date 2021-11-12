8f3545f8a35551a77c3ceb54ecda1449fdb4a1cb
olga210500 <olakovalisko@gmail.com>
2021-11-12 12:02:33 +0200

    fixed errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,11 @@ export default class Home extends Component {
     render() {
         return (
             <><Navibar/>
-            <section id="hero-no-slider" class="d-flex justify-cntent-center align-items-center">
+            <section id="hero-no-slider" className="d-flex justify-cntent-center align-items-center">
                 
-              <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
-                <div class="row justify-content-center">
-                  <div class="col-xl-8">
+              <div className="container position-relative" data-aos="fade-up" data-aos-delay="100">
+                <div className="row justify-content-center">
+                  <div className="col-xl-8">
                     <h2>Welcome to LNUbiz</h2>
                     <p>
                     Одним із важливих завдань при направленні у відрядження працівників є правильне документальне оформлення. Адже бувають випадки, коли з певних причин відрядження скасовують, відтерміновують або змінюють їх тривалість.Для оформлення заявки натисніть кнопку розпочати.
