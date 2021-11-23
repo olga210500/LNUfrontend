5326f72e377bf386dec63910e4653b3fecb5c1b5
olga210500 <olakovalisko@gmail.com>
2021-11-23 18:08:56 +0200

    format files

--- src/App.js
+++ src/App.js
@@ -1,60 +1,42 @@
-import './App.css';
-import Home from './pages/Home';
-import { Route,BrowserRouter ,Switch} from 'react-router-dom';
-import Contact from './pages/Contact';
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Contact from "./pages/Contact";
 // import About from './pages/About';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
-import Application from './pages/Application';
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Application from "./pages/Application";
 function App() {
   const fields = [
     {
-      path:'/about',
-      component:Application
-
-                      
+      path: "/about",
+      component: Application,
     },
     {
-      path:'/contact',
-      component:Contact
-      
+      path: "/contact",
+      component: Contact,
     },
     {
-      path:'/signup',
-      component:Signup
+      path: "/signup",
+      component: Signup,
     },
     {
-      path:'/signin',
-      component:Signin
-    }
-  
+      path: "/signin",
+      component: Signin,
+    },
   ];
-  
+
   return (
-      <>  
-    
-          
-    
-
-          
-          <BrowserRouter>
-         
-           <Switch>
-
-            <Route exact path='/' component={Home} />
-            {fields.map(({ path,component}) => (
-                    <Route path={path} component={component}/>
-                  ))}
-            </Switch>
-   
-
-
-          </BrowserRouter>
-      
-    
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {fields.map(({ path, component }) => (
+            <Route path={path} component={component} />
+          ))}
+        </Switch>
+      </BrowserRouter>
     </>
-    
-        
   );
 }
 
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,61 +1,54 @@
+const setUser = (payload) => ({ type: "SET_USER", payload });
+const URL = "https://localhost:5001/api";
+export const logUserOut = () => ({ type: "LOG_OUT" });
 
-const setUser = (payload) => ({ type: "SET_USER", payload})
-const URL='https://localhost:5001/api'
-export const logUserOut = () => ({type: "LOG_OUT"})
+export const fetchUser = (userInfo) => (dispatch) => {
+  fetch(`${URL}/Login/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+      dispatch(setUser(data.user));
+    });
+};
 
-
-export const fetchUser = (userInfo) => dispatch => {
-    fetch(`${URL}/Login/signin`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(userInfo)
-    })
-    .then(res => res.json() )
-    .then(data => {
-       
-        localStorage.setItem("token", data.token)
-        dispatch(setUser(data.user))
-    })
-}
-
-export const signUserUp = (userInfo) => dispatch => {
-    fetch(`${URL}/Auth/signup`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(userInfo)
-    })
-    .then(res => res.json())
-    .then(data => {
-    
-        localStorage.setItem("token", data.token)
-        dispatch(setUser(data.user))
-    })
-}
+export const signUserUp = (userInfo) => (dispatch) => {
+  fetch(`${URL}/Auth/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+      dispatch(setUser(data.user));
+    });
+};
 
 export const logOut = () => {
-    return dispatch => {
-      const token = localStorage.token;
-      if (token) {
-        return fetch(`${URL}​/Login​/logout`, {
-          method: "GET",
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            
-          }
-        })
-          .then(resp => resp.json())
-          .then(data => {
-          
-              localStorage.removeItem("token")
-            
-          })
-      }
+  return (dispatch) => {
+    const token = localStorage.token;
+    if (token) {
+      return fetch(`${URL}​/Login​/logout`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          localStorage.removeItem("token");
+        });
     }
-  }
+  };
+};
