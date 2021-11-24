8b1b514da9df40b84e31ef7ae3b45e994cdd4e47
olga210500 <olakovalisko@gmail.com>
2021-11-24 21:33:36 +0200

    optimize navibar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -2,37 +2,49 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
-
 import "../styles/style.css";
-
-export default function Navibar() {
-  return (
-    <Navbar collapseOnSelect expand="lg" variant="dark" id="header">
-      <Container>
-        <Navbar.Brand className="logo pb-2">LNUbiz</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+const params = [
+  { to: "/", name: "Home" },
+  {
+    to: "/about",
+    name: "About",
+  },
+  {
+    to: "/contact",
+    name: "Contact",
+  },
+];
+const paramps2 = [
+  { to: "/signin", name: "Sign in" },
+  { to: "/signup", name: "Sign up" },
+];
+const Navibar = () => (
+  <Navbar collapseOnSelect expand="lg" variant="dark" id="header">
+    <Container>
+      <Navbar.Brand className="logo pb-2">LNUbiz</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
           <Nav className="me-auto">
-            <Nav className="pb-2">
-              <Link to="/"> Home </Link>
-            </Nav>
-            <Nav className="pb-2">
-              <Link to="/about"> About </Link>
-            </Nav>
-            <Nav className="pb-2">
-              <Link to="/contact"> Contact </Link>
-            </Nav>
+            {params.map(({ to, name }) => (
+              <Nav className="pb-2">
+                <Link to={to}> {name} </Link>
+              </Nav>
+            ))}
           </Nav>
-          <Nav>
-            <Nav className="pb-2">
-              <Link to="/signin"> Sign in </Link>
-            </Nav>
-            <Nav className="pb-2">
-              <Link to="/signup"> Sign up </Link>
-            </Nav>
+        </Nav>
+
+        <Nav>
+          <Nav className="me-auto">
+            {paramps2.map(({ to, name }) => (
+              <Nav className="pb-2">
+                <Link to={to}> {name} </Link>
+              </Nav>
+            ))}
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+export default Navibar;
