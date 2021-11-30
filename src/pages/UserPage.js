63c990d25ce72006270df0b6fab6bcd48ef7c724
olga210500 <olakovalisko@gmail.com>
2021-11-30 20:20:06 +0200

    Create empty user page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,10 +1,5 @@
-import React from 'react'
 
-export default function UserPage() {
-    let aplacations=[{name:'lala',city:'Lviv'},]
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const UserPage=()=>(
+    <p>User Page</p>
+)
+export default UserPage;
