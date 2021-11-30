e045b6d8595548b3aae832812c94bf6d1e51b505
olga210500 <olakovalisko@gmail.com>
2021-11-30 20:16:29 +0200

    Optimize application page

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -1,75 +1,35 @@
-import Navibar2 from "../components/Navibar2";
+import Navibar from "../components/Navibar";
 import "../styles/style.css";
-import { connect } from "react-redux";
-import { signUserUp } from "../actions/userActions";
 import { useState } from "react";
 import InputField from "../components/InputField";
-const fieldsForInput = [
-  {
-    title: "Прізвище, ім'я, по батькові (в родовому відмінку)",
-    type: "text",
-    name: "fullName",
-    id: "fullName",
-    placeholder: "Прізвище, ім'я, по батькові",
-  },
-  {
-    title: "Вкажіть підрозділ та посаду за основним місцем праці",
-    type: "text",
-    name: "",
-    id: "",
-    placeholder: "Ваша відповідь",
-  },
-  {
-    title: "Вкажіть відділ та посаду за сумісництвом (за наявності)",
-    type: "text",
-    name: "",
-    id: "",
-    placeholder: "Ваша відповідь",
-  },
-  {
-    title: "Вкажіть місто відрядження",
-    type: "text",
-    name: "",
-    id: "",
-    placeholder: "Ваша відповідь",
-  },
-  {
-    title: "Вкажіть країну відрядження (для закордонного відрядження)",
-    type: "text",
-    name: "",
-    id: "",
-    placeholder: "Ваша відповідь",
-  },
-  {
-    title: "Вкажіть маршрут поїздки (для відряджень по Україні). Наприклад: Львів-Київ-Львів",
-    type: "text",
-    name: "",
-    id: "",
-    placeholder: "Ваша відповідь",
-  },
-];
+import fieldsForInput from "../variblesForApplication/fieldsForInput";
+import SubmitButton from "../components/submitButton";
+import radioFileds from "../variblesForApplication/radioFields";
+
+
 const Application = () => {
   const [state, setState] = useState({
     fullName: "",
     Date: new Date(),
-    Status: "in progress",
-    FullTimePosition: "",
-    PartTimePosition: "",
+    Status: "не підтверджена",
+    FullTimePosition: "", // повна зайнятість
+    PartTimePosition: "", // за сумісництвом
     IsAbroadTrip: 0,
     Purpose: "",
-    RetentionType: 1,
+    RetentionType: 1, //повернення коштів
     City: "",
     Country: "",
-    Institution: "",
+    Institution: "", //заклад куди направляєтесь
     StartDate: new Date(),
     EndDate: new Date(),
-    Route: "",
-    Transport: "",
+    Route: "", //маршут
+    Transport: "", //нема в базі
     ExpensesPayment: "",
     TripReason: "",
   });
 
   const handleOnChange = (fieldName) => (fieldValue) => {
+  
     setState(() => ({
       [fieldName]: fieldValue,
     }));
@@ -77,13 +37,20 @@ const Application = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-  
+
     console.log(state);
   };
+  const onChangeRadioButton = (buttonName) => (buttonValue) => {
+    setState(() => ({
+      [buttonName]: buttonValue,
+    }));
+    
+  };
+
 
   return (
     <>
-      <Navibar2 />
+      <Navibar/>
       <section className="contact">
         <div className="d-flex flex-column justify-content-center">
           <div className="titleContainer ">
@@ -104,120 +71,27 @@ const Application = () => {
                   />
                 </div>
               ))}
-
-              <div className="col-md-5 php-email-form mt-3 mt-md-0 mb-3 columnApp">
-                <h6>Вкажіть тип відрядження</h6>
-                <div className="form-check">
-                  <input type="radio" name="typeTrip" id="" />
-                  <label className="form-check-label px-2" htmlFor="typeTrip">
-                    <h6> Відрядження по Україні </h6>
-                  </label>
-                </div>
-                <div className="form-check">
-                  <input type="radio" name="typeTrip" id="" />
-                  <label className="form-check-label px-2" htmlFor="typeTrip">
-                    <h6> Відрядження за кордон </h6>
-                  </label>
-                </div>
-              </div>
-
-              <div className="col-md-5 php-email-form mt-3 mt-md-0 mb-3 columnApp">
-                <h6>Вкажіть вид відрядження</h6>
-                <div className="form-check">
-                  <div className="header_6">
-                    <input
-                      className="float-left"
-                      type="radio"
-                      name="purposeTrip"
-                      id=""
-                    />
-                    <label
-                      className="form-check-label px-2"
-                      htmlFor="purposeTrip"
-                    >
-                      <h6>
-                        З метою реалізації права на мобільність відповідно до
-                        Постанови Кабінету Міністрів України від 12.08.2019 р.
-                        №579
-                      </h6>
-                    </label>
+              {radioFileds.map(({ name, title, options }) => (
+                <div className="col-md-5 php-email-form mt-3 mt-md-0 mb-3 columnApp">
+                  <h6>{title}</h6>
+                  <div onChange={onChangeRadioButton}>
+                    {options.map(({ label, value }) => (
+                      <div className="form-check">
+                        <div className="header_6">
+                          <input type="radio" name={name} value={value} />
+                          <label
+                            className="form-check-label px-2 mt-2"
+                            htmlFor={name}
+                          >
+                            <h6> {label} </h6>
+                          </label>
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
-                <div className="form-check">
-                  <div className="header_6">
-                    <input type="radio" name="purposeTrip" id="" />
-                    <label
-                      className="form-check-label px-2"
-                      htmlFor="purposeTrip"
-                    >
-                      <h6> проходження стажування </h6>
-                    </label>
-                  </div>
-                </div>
-                <div className="form-check">
-                  <div className="header_6">
-                    <input type="radio" name="purposeTrip" id="" />
-                    <label
-                      className="form-check-label px-2"
-                      htmlFor="purposeTrip"
-                    >
-                      <h6> для викладу лекційного курсу </h6>
-                    </label>
-                  </div>
-                </div>
-                <div className="form-check">
-                  <div className="header_6">
-                    <input type="radio" name="purposeTrip" id="" />
-                    <label
-                      className="form-check-label px-2"
-                      htmlFor="purposeTrip"
-                    >
-                      <h6> участь в проектній зустрічі </h6>
-                    </label>
-                  </div>
-                </div>
-                <div className="form-check">
-                  <div className="header_6">
-                    <input type="radio" name="purposeTrip" id="" />
-                    <label
-                      className="form-check-label px-2"
-                      htmlFor="purposeTrip"
-                    >
-                      <h6> участь у тренінгу </h6>
-                    </label>
-                  </div>
-                </div>
-
-                <div className="form-check">
-                  <div>
-                    <input type="radio" name="purposeTrip" id="" />
-                    <label
-                      className="form-check-label px-2"
-                      htmlFor="purposeTrip"
-                    >
-                      <h6> інше </h6>
-                    </label>
-                    <input type="text" name="purposeTrip" id=""></input>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-md-5 php-email-form mt-3 mt-md-0 mb-3 columnApp">
-                <h6></h6>
-                <input
-                  type="text"
-                  className="form-control"
-                  name=""
-                  id=""
-                  placeholder="Your Email"
-                  required
-                />
-              </div>
-              <div className="text-center ">
-                <button type="submit" className="rounded">
-                  Надіслати
-                </button>
-              </div>
+              ))}
+              <SubmitButton title='Надіслати' />
             </form>
           </div>
         </div>
@@ -226,10 +100,8 @@ const Application = () => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    signUserUp: (userInfo) => dispatch(signUserUp(userInfo)),
-  };
-};
 
-export default connect(null, mapDispatchToProps)(Application);
+
+
+export default Application;
+
