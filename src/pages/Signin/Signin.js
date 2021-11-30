59ef95ed0721fbe27a4f61e426854bd997a97693
olga210500 <olakovalisko@gmail.com>
2021-11-30 20:17:09 +0200

    Create signin page wich combine two components

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,7 +1,4 @@
-import React, { Component } from "react";
-import Navibar2 from "../../components/Navibar2";
 import "../../styles/style.css";
-import InputField from "../../components/InputField";
 import { connect } from "react-redux";
 import SignInComponent from "./SignInComponent";
 import InformationLoggedIn from "../errors/loggedInError";
