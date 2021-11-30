8cfad8a9c26720640b286368768bf81aa9561dcb
olga210500 <olakovalisko@gmail.com>
2021-11-30 20:15:15 +0200

    Add posibilities from second navibar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -3,8 +3,14 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import "../styles/style.css";
+import { connect } from "react-redux";
+import Logout from "./Logout";
+
 const params = [
-  { to: "/", name: "Home" },
+  {
+    to: "/",
+    name: "Home",
+  },
   {
     to: "/about",
     name: "About",
@@ -18,7 +24,8 @@ const paramps2 = [
   { to: "/signin", name: "Sign in" },
   { to: "/signup", name: "Sign up" },
 ];
-const Navibar = () => (
+
+const Navibar = ({ userReducer }) => (
   <Navbar collapseOnSelect expand="lg" variant="dark" id="header">
     <Container>
       <Navbar.Brand className="logo pb-2">LNUbiz</Navbar.Brand>
@@ -27,8 +34,14 @@ const Navibar = () => (
         <Nav className="me-auto">
           <Nav className="me-auto">
             {params.map(({ to, name }) => (
-              <Nav className="pb-2">
-                <Link to={to}> {name} </Link>
+              <Nav
+                className="pb-2"
+                key={Math.random().toString(36).substr(2, 9)}
+              >
+                <Link key={Math.random().toString(36).substr(2, 9) + 1} to={to}>
+                  {" "}
+                  {name}{" "}
+                </Link>
               </Nav>
             ))}
           </Nav>
@@ -36,15 +49,25 @@ const Navibar = () => (
 
         <Nav>
           <Nav className="me-auto">
-            {paramps2.map(({ to, name }) => (
-              <Nav className="pb-2">
-                <Link to={to}> {name} </Link>
-              </Nav>
-            ))}
+            {!userReducer.loggedIn ? (
+              paramps2.map(({ to, name }) => (
+                <Nav className="pb-2">
+                  <Link to={to}> {name} </Link>
+                </Nav>
+              ))
+            ) : (
+              <Logout />
+            )}
           </Nav>
         </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
 );
-export default Navibar;
+const mapStateToProps = (state) => {
+  return {
+    userReducer: state.userReducer,
+  };
+};
+
+export default connect(mapStateToProps)(Navibar);
