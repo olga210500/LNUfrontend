8426e5aade5bb64397094fa957d608e2022da23c
olga210500 <olakovalisko@gmail.com>
2021-12-01 18:01:01 +0200

    Create admin component

---
+++ src/pages/UserPage/AdminPageComponent.js
@@ -0,0 +1,46 @@
+import React from "react";
+class AdminPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://localhost:5001/api/Admin/usersTable',
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
+    )
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+      });
+  }
+
+  render() {
+    const { items } = this.state;
+ 
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              <h3>{item.title}</h3>
+              <p>{item.body}</p>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  
+}
+export default AdminPage;
