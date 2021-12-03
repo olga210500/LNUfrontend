9da401af5f8b824a3513f2046d41ea350d98dbaa
olga210500 <olakovalisko@gmail.com>
2021-12-03 16:56:41 +0200

    Create file for fields for Signin page

---
+++ src/pages/Signin/signInFields.js
@@ -0,0 +1,14 @@
+export const fields = [
+    {
+      type: "email",
+      name: "email",
+      id: "email",
+      placeholder: "Your Email",
+    },
+    {
+      type: "password",
+      name: "password",
+      id: "password",
+      placeholder: "Password",
+    },
+  ];
