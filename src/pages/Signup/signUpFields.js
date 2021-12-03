bba2a2f15f64d5c49d2bc2fa7a9d276d9fe60afd
olga210500 <olakovalisko@gmail.com>
2021-12-03 16:58:58 +0200

    Create file for signedUp fields

---
+++ src/pages/Signup/signUpFields.js
@@ -0,0 +1,34 @@
+export const  signUpFields = [
+    {
+      type: "text",
+      name: "name",
+      id: "name",
+      placeholder: "First name",
+    },
+    {
+      type: "text",
+      name: "surname",
+      id: "surname",
+      placeholder: "Last name",
+    },
+
+    {
+      type: "email",
+      name: "email",
+      id: "email",
+      placeholder: "Your Email",
+    },
+
+    {
+      type: "password",
+      name: "password",
+      id: "password",
+      placeholder: "Password",
+    },
+    {
+      type: "password",
+      name: "confirmPassword",
+      id: "confirmPassword",
+      placeholder: "Confirm password",
+    },
+  ];
