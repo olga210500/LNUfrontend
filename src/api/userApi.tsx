30cc8856331aa1b78c6ea60e2d83fafc8d160883
tania <tanyabiryuk7@gmail.com>
2021-12-06 11:49:19 +0200

    Add userApi to access user personal info

---
+++ src/api/userApi.tsx
@@ -0,0 +1,21 @@
+import AuthStore from '../stores/AuthStore';
+import jwt_decode from 'jwt-decode';
+import Api from './api'
+
+const getById = async (id: string | undefined) => {
+    return await Api.get(`User/${id}`);
+};
+
+const getCurrent = async () => {
+    let jwt = AuthStore.getToken() as string;
+    let decodedJwt = jwt_decode(jwt) as any;
+    return await Api.get(`User/${decodedJwt.nameid}`)
+        .then((response) => {
+        console.log(response)
+    });
+};
+
+export default {
+    getById,
+    getCurrent,
+};
