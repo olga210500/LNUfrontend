8e1691465a50744b2967697c10e2f94a8789cdea
olga210500 <olakovalisko@gmail.com>
2021-12-11 00:49:44 +0200

    Create user page

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,5 +1,24 @@
+import userApi from "../../api/userApi";
+import { useState, useEffect } from "react";
 
-const UserPage=()=>(
-    <p>User Page</p>
-)
+const UserPage = () => {
+  const [userInfo, setuserInfo] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+  });
+  useEffect(() => {
+    userApi.getCurrent().then((res) => {
+      setuserInfo(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p> {userInfo.email}</p>
+    </div>
+  );
+};
+
 export default UserPage;
