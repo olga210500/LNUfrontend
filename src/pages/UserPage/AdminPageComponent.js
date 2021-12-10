83cd4ef24e298b53292059d3e5d12c367b268595
olga210500 <olakovalisko@gmail.com>
2021-12-11 00:48:40 +0200

    Create admin page

--- src/pages/UserPage/AdminPageComponent.js
+++ src/pages/UserPage/AdminPageComponent.js
@@ -1,56 +1,81 @@
-import React from "react";
-import userApi from "../../api/userApi";
+import adminActions from "../../actions/adminActions";
+import { useState, useEffect } from "react";
+import "../../styles/style.css";
+// import DatatablePage from "./TableComponent";
+import { MDBDataTableV5 } from "mdbreact";
+import { Container } from "react-bootstrap";
+import Navibar from "../../components/Navibar";
 
+const AdminPage = () => {
+  const [datatable, setusersInfo] = useState({
+    columns: [
+      {
+        label: "Прізвище",
+        field: "lastName",
+        width: "10vh",
+        attributes: {
+          "aria-controls": "DataTable",
+          "aria-label": "FirstName",
+        },
+      },
+      {
+        label: "Ім'я",
+        field: "firstName",
+        width: 170,
+        sort: "disabled",
+      },
+      {
+        label: "По батькові",
+        field: "fatherName",
+        width: 200,
+        sort: "disabled",
+      },
+      {
+        label: "Телефон",
+        field: "phoneNumber",
+        width: 100,
+        sort: "disabled",
+      },
+      {
+        label: "Email",
+        field: "email",
+        width: 150,
+      },
 
-class AdminPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-    };
-  }
+      {
+        label: "Ім'я користувача",
+        field: "userName",
+        width: 150,
+      },
+    ],
 
-  async componentDidMount() {
-    const response = userApi.getCurrent();
+    rows: [],
+  });
+  useEffect(() => {
+    adminActions.getUsersTable().then((res) => {
+      setusersInfo({ ...datatable, rows: [res.data].flat() });
+    });
+  }, []);
 
-    // fetch(`${`https://localhost:5001/User/`}${decodedJwt.nameid}`,
-    // {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Accept: "application/json",
-    //   },
-    // }
-    // )
-    //   .then(res => {
-    //     const result=res.json();
-    //     console.log("sssss", result);
-    //     return result;
-    //   })
-    //   .then(result => {
-    //     console.log(result);
-    //     this.setState({
-    //       isLoaded: true,
-    //       items: result
-    //     });
-    //   });
-  }
+  return (
+    <>
+      <Navibar />
+      <div className="table">
+        <Container>
+          <MDBDataTableV5
+            hover
+            responsive
+            entriesOptions={[5, 20, 25]}
+            entries={5}
+            pagesAmount={4}
+            data={datatable}
+            materialSearch
+          />
+        </Container>
+      </div>
+    </>
+    // <div className="d-flex">{console.log(usersInfo.users_info)}<DatatablePage data={usersInfo.users_info}/></div>
+  );
+};
 
-  render() {
-    const { items } = this.state;
- 
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              <h3>{item.title}</h3>
-              <p>{item.body}</p>
-            </li>
-          ))}
-        </ul>
-      );
-    }
-  
-}
 export default AdminPage;
