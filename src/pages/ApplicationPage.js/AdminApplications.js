d2d63061cf9347a0065a5c6e98393ff8bea18e01
olga210500 <olakovalisko@gmail.com>
2021-12-12 14:38:50 +0200

    Admin page with all aplications

--- src/pages/ApplicationPage.js/AdminApplications.js
+++ src/pages/ApplicationPage.js/AdminApplications.js
@@ -1,3 +1,39 @@
-const UserApplicationPage=()=>{
+import { useEffect } from "react";
+import { getApplicationsTable } from "../../actions/adminAction";
+
+const UsersApplicationPage=()=>{
     
+    const [datatable, setApplication] = useState({
+        columns:InitialStates.ApplicationColumns,
+        rows: [],
+      });
+      
+
+    useEffect(()=>{
+        getApplicationsTable().then((res)=>{
+            setApplication({ ...datatable, rows: [res.data].flat() });
+
+        })
+
+    })
+    
+      return (
+        <>
+          <Navibar />
+          <div className="table">
+            <Container>
+              <MDBDataTableV5
+                hover
+                responsive
+                entriesOptions={[5, 20, 25]}
+                entries={5}
+                pagesAmount={4}
+                data={datatable}
+                materialSearch
+              />
+            </Container>
+          </div>
+        </>
+      );
 }
+export default UsersApplicationPage
